147ead38145135bb5f5508979162da7f8cdc26eb
Robert <rtudor@palmcentre.co.uk>
2021-06-01 19:15:38 +0100

    attempting multiple messages in a single response

--- src/XmlResponse.php
+++ src/XmlResponse.php
@@ -3,6 +3,35 @@ namespace Mcpuishor\OrderwiseApi;
 
 class XmlResponse 
 {
+	public $messages;
+
+	public function __construct()
+	{
+		$this->messages = [];
+		return $this;
+	}
+
+	public function add(string $message, string $type = "Log")
+	{
+		array_push($this->messages,[
+			"Type" => $type,
+			"Message" => $message,
+		]);
+		return $this;
+	}
+
+	public function get()
+	{
+		return response()->xml(
+			[
+				'Responses' => [
+					'Response' => [
+						$this->messages,
+					],
+				]
+			],
+		200, [], "XMLFile");
+	}
 	
 	static public function with(string $message, string $type = "Log")
 	{
